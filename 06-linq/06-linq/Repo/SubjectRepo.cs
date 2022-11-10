69e6c51bb7ce96c38133285e9d57ed2e5bf8113d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-11-10 14:41:04 +0100

    Subjects repo

---
+++ 06-linq/06-linq/Repo/SubjectRepo.cs
@@ -0,0 +1,28 @@
+﻿using _06_linq.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_linq.Repo
+{
+    public class SubjectRepo
+    {
+        private List<Subject> subjects;
+
+        public List<Subject> Subjects { get => subjects; set => subjects = value; }
+
+        public SubjectRepo()
+        {
+            subjects.Add(new Subject(1, "Matematika"));
+            subjects.Add(new Subject(2, "Magyar nyelv"));
+            subjects.Add(new Subject(2, "Irodalom"));
+            subjects.Add(new Subject(2, "Angol nyelv"));
+            subjects.Add(new Subject(2, "Német nyelv"));
+            subjects.Add(new Subject(2, "Történelem"));
+            subjects.Add(new Subject(2, "Rajz"));
+            subjects.Add(new Subject(2, "Testnevelés"));
+        }
+    }
+}
