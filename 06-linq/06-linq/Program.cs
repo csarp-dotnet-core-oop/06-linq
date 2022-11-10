67f9f752d1f8e2e38879dfa8222d52cb83fbe243
gycsaba <gyuris.csaba@vasvari.org>
2022-11-10 20:57:25 +0100

    Tanár tantárgy párok

--- 06-linq/06-linq/Program.cs
+++ 06-linq/06-linq/Program.cs
@@ -1,87 +1,17 @@
-﻿using System.Text;
+﻿using _06_linq.Service;
+using System.Text;
 
-namespace Kutyák
+namespace _06_linq
 {
-    class Vizsgalat
-    {
-        string nev;
-        string fajta;
-        DateTime utolsoVizsgalat;
-        int eletkor;
-
-        public string Nev
-        {
-            get
-            {
-                return this.nev;
-            }
-
-            set
-            {
-                this.nev = value;
-            }
-        }
-        public string Fajta
-        {
-            get
-            {
-                return this.fajta;
-            }
-
-            set
-            {
-                this.fajta = value;
-            }
-        }
-        public DateTime UtolsoVizsgalat
-        {
-            get
-            {
-                return this.utolsoVizsgalat;
-            }
-
-            set
-            {
-                this.utolsoVizsgalat = value;
-            }
-        }
-        public int Eletkor
-        {
-            get
-            {
-                return this.eletkor;
-            }
-
-            set
-            {
-                this.eletkor = value;
-            }
-        }
-
-        public Vizsgalat(string sor, Dictionary<int, string> kutyafajták, Dictionary<int, string> kutyanevek)
-        {
-            string[] adatok = sor.Split(';');
-            this.Fajta = kutyafajták[Convert.ToInt32(adatok[1])];
-            this.Nev = kutyanevek[Convert.ToInt32(adatok[2])];
-            this.Eletkor = Convert.ToInt32(adatok[3]);
-            this.UtolsoVizsgalat = Convert.ToDateTime(adatok[4]);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> kutyanevek = new Dictionary<int, string>();
-            kutyanevek = File.ReadAllLines("../../../KutyaNevek.csv", Encoding.UTF8).Skip(1).ToDictionary(sor => Convert.ToInt32(sor.Split(';')[0]), sor => sor.Split(';')[1]);
-            Dictionary<int, string> kutyafajták = new Dictionary<int, string>();
-            kutyafajták = File.ReadAllLines("../../../KutyaFajták.csv", Encoding.UTF8).Skip(1).ToDictionary(sor => Convert.ToInt32(sor.Split(';')[0]), sor => sor.Split(';')[1]);
 
-            List<Vizsgalat> vizsgálatok = File.ReadAllLines("../../../Kutyák.csv", Encoding.UTF8).Skip(1).Select(sor => new Vizsgalat(sor, kutyafajták, kutyanevek)).ToList();
-
-            
+            TeachTeacherSubjectService teachTeacherSubjectService = new TeachTeacherSubjectService();
+            //teachTeacherSubjectService.TeacherSubjectPair();
+            teachTeacherSubjectService.TeacherSubjectPairLinq();
             Console.ReadKey();
-
         }
     }
 }
--- 06-linq/06-linq/Service/TeachTeacherSubjectService.cs
+++ 06-linq/06-linq/Service/TeachTeacherSubjectService.cs
@@ -1,4 +1,5 @@
-﻿using _06_linq.Repo;
+﻿using _06_linq.Model.DTO;
+using _06_linq.Repo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace _06_linq.Service
 {
+    // https://dotnettutorials.net/lesson/introduction-to-linq/
     public class TeachTeacherSubjectService
     {
         RepositoryWrapper repositoryWrapper;
@@ -17,7 +19,37 @@ namespace _06_linq.Service
         }
 
         // 1. feladat
-        // 
+        // Tanár tantárgy párok
+        public void TeacherSubjectPair()
+        {
+            List<TeacherSubjectPair> result = (from teacher in repositoryWrapper.TeacherRepo.Teachers
+                                               from subject in repositoryWrapper.SubjectRepo.Subjects
+                                               orderby teacher.Name
+                                               select new TeacherSubjectPair
+                                               {
+                                                   Teacher = teacher.Name,
+                                                   Subject = subject.Name                                                   
+                                               }).ToList();
+
+            Console.WriteLine("Tanár - tantárgy párok:");
+            foreach (TeacherSubjectPair pair in result)
+                Console.WriteLine($"{pair.Teacher} => {pair.Subject}");
+        }
+
+        public void TeacherSubjectPairLinq()
+        {
+            var result = repositoryWrapper.TeacherRepo.Teachers.SelectMany(
+                    teacher => repositoryWrapper.SubjectRepo.Subjects, (teacher, subject) => new TeacherSubjectPair
+                    {
+                        Teacher = teacher.Name,
+                        Subject = subject.Name
+                    }).OrderBy(pair => pair.Teacher).ToList();
+                                     
+
+            Console.WriteLine("Tanár - tantárgy párok:");
+            foreach (TeacherSubjectPair pair in result)
+                Console.WriteLine($"{pair.Teacher} => {pair.Subject}");
+        }
 
     }
 }
