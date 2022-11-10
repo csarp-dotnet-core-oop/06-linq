4db50c3689931fcab4240e1cacab0c109b53fa58
gycsaba <gyuris.csaba@vasvari.org>
2022-11-10 19:10:02 +0100

    Service réteg

---
+++ 06-linq/06-linq/Service/TeachTeacherSubjectService.cs
@@ -0,0 +1,23 @@
+﻿using _06_linq.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_linq.Service
+{
+    public class TeachTeacherSubjectService
+    {
+        RepositoryWrapper repositoryWrapper;
+
+        public TeachTeacherSubjectService()
+        {
+            repositoryWrapper = new RepositoryWrapper();
+        }
+
+        // 1. feladat
+        // 
+
+    }
+}
