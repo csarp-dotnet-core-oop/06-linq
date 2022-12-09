72e1985423e2f2170aae1a594fc7a1bad8b693b7
gycsaba <gyuris.csaba@vasvari.org>
2022-12-09 20:06:25 +0100

    Ötös és hatos dotnet

---
+++ 06-linq-dotnet-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06_linq_dotnet_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
