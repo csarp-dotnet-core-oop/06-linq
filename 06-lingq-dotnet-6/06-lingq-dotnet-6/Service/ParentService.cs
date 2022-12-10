c6baf0e371659a73262e0a304da6f960d60fcdd2
gycsaba <gyuris.csaba@vasvari.org>
2022-12-10 18:56:52 +0100

    A feladat még fejleszthető

---
+++ 06-lingq-dotnet-6/06-lingq-dotnet-6/Service/ParentService.cs
@@ -0,0 +1,30 @@
+﻿using _06_linq.Model;
+using _06_linq_dotnet_5.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_linq_dotnet_5.Service
+{
+    class ParentService
+    {
+        private ParentRepo parentRepo;
+
+        public ParentService()
+        {
+            parentRepo = new ParentRepo();
+        }
+
+        public void ParentWomanLinq()
+        {
+            //IEnumerable<Parent> parent= from 
+        }
+
+        public void ParantWomanLambda()
+        {
+            //List<Parent> parent=parentRepo.Parents
+        }
+    }
+}
